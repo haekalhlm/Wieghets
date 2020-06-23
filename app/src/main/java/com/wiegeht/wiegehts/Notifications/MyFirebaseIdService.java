f0da76834562ec7b8da750f8142d4a849b302114
haekalhlm <haekal.hlm@gmail.com>
2020-06-23 15:00:08 +0700

    Tugas Algo

---
+++ app/src/main/java/com/wiegeht/wiegehts/Notifications/MyFirebaseIdService.java
@@ -0,0 +1,35 @@
+package com.wiegeht.wiegehts.Notifications;
+
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+
+class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("NEW_TOKEN",s);
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String refreshToken = FirebaseInstanceId.getInstance().getToken();
+        if (firebaseUser != null){
+            updateToken(refreshToken);
+        }
+    }
+
+    private void updateToken(String refreshToken) {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
+        Token token = new Token(refreshToken);
+        reference.child(firebaseUser.getUid()).setValue(token);
+    }
+}
---
+++ app/src/main/java/com/wiegeht/wiegehts/Notifications/MyResponse.java
@@ -0,0 +1,6 @@
+package com.wiegeht.wiegehts.Notifications;
+
+public class MyResponse {
+
+    public int success;
+}
